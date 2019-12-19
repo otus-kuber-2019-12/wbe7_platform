d7c273fc5111aac4bc2badd3ad0426be073a64a7
Mikhail Tikhonov <wbe7@ya.ru>
2019-12-19 16:57:44 +0300

    Some change for golang web-app

--- kubernetes-intro/web/main.go
+++ kubernetes-intro/web/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from K8S cluster \n")
+	fmt.Fprintf(w, "Your user-agent parameters: %v\n", r)
+	time := time.Now()
+	fmt.Fprintf(w, "Time: %v", time)
+}
 
 func main() {
 	http.HandleFunc("/_healthz", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "OK")
 	})
+	http.HandleFunc("/hello", hello)
 	fs := http.FileServer(http.Dir("/app/"))
 	http.Handle("/", http.StripPrefix("/", fs))
 	http.ListenAndServe(":8000", nil)
